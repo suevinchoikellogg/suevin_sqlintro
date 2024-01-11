c960c3ed3fca7f0fbb8d67af2a6e0801cf97a27c
suevin choi <suevin.choi@kellogg.northwestern.edu>
2024-01-11 02:01:08 +0000

    complete lab 2

--- demo.sql
+++ demo.sql
@@ -2,9 +2,18 @@
 
 SELECT first_name, last_name FROM players;
 
+[ Lab 1 ]
 -- sqlite3 babeball.sqlite3
 -- .schema
 -- .mode columns
 -- .headers on
 -- .read __(file name)___
 -- .quit
+
+[ Lab 2 ]
+SELECT _____ FROM _____ 
+WHERE _____ 
+AND _____ 
+ORDER BY _____ 
+LIMIT _____;
+
